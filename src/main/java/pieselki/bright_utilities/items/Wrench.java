cd50ce28e8153de2ef3a8491e2c4c6cc255b9470
Tismas <adamski256@gmail.com>
2021-04-08 18:55:59 +0200

    feat: wrench en translations and textures [PIES-1]

--- src/main/java/pieselki/bright_utilities/items/Wrench.java
+++ src/main/java/pieselki/bright_utilities/items/Wrench.java
@@ -1,26 +1,16 @@
 package pieselki.bright_utilities.items;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
-import com.mojang.authlib.properties.Property;
-import com.mojang.datafixers.util.Pair;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.DirectionalBlock;
-import net.minecraft.block.PistonBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
-import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.vector.Orientation;
 import net.minecraft.world.World;
-import pieselki.bright_utilities.BrightUtilities;
 
 public class Wrench extends Item {
     public Wrench(Item.Properties properties) {
