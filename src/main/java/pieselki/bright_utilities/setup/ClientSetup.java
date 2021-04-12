87e9d2e52f300f6df41166d7328d1fd32537b5f6
Tismas <adamski256@gmail.com>
2021-04-12 20:19:47 +0200

    fix registration of chat event

--- src/main/java/pieselki/bright_utilities/setup/ClientSetup.java
+++ src/main/java/pieselki/bright_utilities/setup/ClientSetup.java
@@ -1,13 +1,15 @@
 package pieselki.bright_utilities.setup;
 
 import pieselki.bright_utilities.BrightUtilities;
+import pieselki.bright_utilities.events.ChatEvents;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = BrightUtilities.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientSetup {
     public static void init(final FMLClientSetupEvent event) {
-
+        MinecraftForge.EVENT_BUS.register(new ChatEvents());
     }
 }
